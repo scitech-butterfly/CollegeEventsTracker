6b189439da36908491485dca569073fae9eaa19c
prishajaiswal75 <164599457+prishajaiswal75@users.noreply.github.com>
2025-04-01 17:53:44 +0530

    Update EventManagement.java By creating Event class

--- import java.util.*; /EventManagement.java
+++ import java.util.*; /EventManagement.java
@@ -6,3 +6,27 @@ interface EventManagement {
     ArrayList<Event> searchEventsByName(String name);
     ArrayList<Event> getEventsForMonth(int month, int year);
 }
+// Event class
+class Event {
+    private final String eventId; 
+    private String name;
+    private String description;
+    private String venue;
+    private String date;
+    private String time;
+    private String communityId;
+    private boolean membersOnly;
+    private ArrayList<String> attendees;
+
+    public Event(String eventId, String name, String description, String venue,
+                 String date, String time, String communityId, boolean membersOnly) {
+        this.eventId = eventId;
+        this.name = name;
+        this.description = description;
+        this.venue = venue;
+        this.date = date;
+        this.time = time;
+        this.communityId = communityId;
+        this.membersOnly = membersOnly;
+        this.attendees = new ArrayList<>();
+    }
