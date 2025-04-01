5bc270139f37b643e5f491c7bf1e216acb4ee4e7
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-04-01 14:36:12 +0530

    Create Department.java

---
+++ Department.java
@@ -0,0 +1,8 @@
+// Department class (hierarchical inheritance)
+class Department extends Community {
+    private String facultyHead;
+
+    public Department(String communityId, String name, String facultyHead) {
+        super(communityId, name);
+        this.facultyHead = facultyHead;
+    }
