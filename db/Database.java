44d88e7f9994229aa9bab6a7e83201ef531aedc8
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-21 00:02:37 +0530

    Added method to establish connection with MySQL database

--- db/Database.java
+++ db/Database.java
@@ -1,1 +1,21 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+  private static Connection connection;
+
+  // Establish connection with MySQL database
+  public static Connection getConnection() throws SQLException {
+    if (connection == null || connection.isClosed()) {
+      connection = DriverManager.getConnection(
+          "jdbc:mysql://localhost:3306/college_events",
+          "root",
+          "Password@123");
+    }
+    return connection;
+  }
+}
 
