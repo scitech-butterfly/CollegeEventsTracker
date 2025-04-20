bb92b814d7d13952f622137f63c8e9726cc48a47
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-04-21 02:13:56 +0530

    Implement Student class with role and event permissions

--- Student.java
+++ Student.java
@@ -1,1 +1,19 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class Student extends User {
+
+    public Student(int userId, String name, String email) {
+        super(userId, name, email, "student");
+    }
+
+    @Override
+    public boolean canAddEvents() {
+        return false;
+    }
+
+    @Override
+    public void displayProfile() {
+        super.displayProfile();
+    }
