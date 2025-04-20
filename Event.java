56f5a77ea0f9510ace44710c0963cbf2db2169b7
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-21 00:08:37 +0530

    Added Parameterized constructor of Event

--- Event.java
+++ Event.java
@@ -11,5 +11,17 @@ public class Event {
   private String venue;
   private int communityID;
   private boolean membersOnly;
+
+  public Event(int id, String name, String description, String venue, Date date, Time time, int communityID,
+      boolean membersOnly) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.venue = venue;
+    this.date = date;
+    this.time = time;
+    this.communityID = communityID;
+    this.membersOnly = membersOnly;
+  }
 }
 
