a0534aee79adfdbd0e318909160581691ba55d92
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-21 00:07:55 +0530

    Added class Event and its members

--- Event.java
+++ Event.java
@@ -1,1 +1,15 @@
+// Event.java
+import java.sql.Date;
+import java.sql.Time;
+
+public class Event {
+  private int id;
+  private String name;
+  private String description;
+  private Date date;
+  private Time time;
+  private String venue;
+  private int communityID;
+  private boolean membersOnly;
+}
 
