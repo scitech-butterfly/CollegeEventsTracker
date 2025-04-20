b31aafaa3d3c3a9f266cf7fe5b08fb9347a7ce9c
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-04-21 02:10:21 +0530

    Define abstract User class with constructor

--- User.java
+++ User.java
@@ -1,1 +1,13 @@
+public abstract class User {
+    protected final int userId; // now int (matches SQL schema)
+    protected String name;
+    protected String email;
+    protected String role;
 
+    // Constructor
+    public User(int userId, String name, String email, String role) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
